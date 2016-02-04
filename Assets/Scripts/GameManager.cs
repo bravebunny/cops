0bf997d32e17f0ab429104dc22beb350fce95e35
Francisco Baio Dias <xicombd@gmail.com>
2016-02-04 20:36:35 +0000

    Add game manager to spawn cops every x time

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityStandardAssets.Vehicles.Car;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject CopPrefab;         
+	public GameObject Player;         
+	public float spawnInterval;
+
+	private List<GameObject> Cops = new List<GameObject> ();
+
+	private void Start()
+	{
+		InvokeRepeating ("SpawnCop", 0, spawnInterval);
+	}
+
+
+	private void SpawnCop()
+	{
+		Vector3 spawnPosition = Player.GetComponent<Rigidbody> ().position;
+		spawnPosition.y += 10;
+
+		GameObject cop = Instantiate(CopPrefab, spawnPosition, Player.GetComponent<Rigidbody>().rotation) as GameObject;
+
+		cop.GetComponent<CarAIControl>().SetTarget(Player.transform);
+
+		Cops.Add (cop);
+	}
+
+}
