485643ca23b3ebaa326142cef084bd590132dba0
Francisco Baio Dias <xicombd@gmail.com>
2016-02-04 22:28:12 +0000

    Add big area map, disable wheel effects

--- Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
+++ Assets/Standard Assets/Vehicles/Car/Scripts/CarController.cs
@@ -270,7 +270,8 @@ namespace UnityStandardAssets.Vehicles.Car
         // these effects are controlled through the WheelEffects class
         private void CheckForWheelSpin()
         {
-            // loop through all wheels
+			return;
+			// loop through all wheels
             for (int i = 0; i < 4; i++)
             {
                 WheelHit wheelHit;
@@ -279,7 +280,7 @@ namespace UnityStandardAssets.Vehicles.Car
                 // is the tire slipping above the given threshhold
                 if (Mathf.Abs(wheelHit.forwardSlip) >= m_SlipLimit || Mathf.Abs(wheelHit.sidewaysSlip) >= m_SlipLimit)
                 {
-                    m_WheelEffects[i].EmitTyreSmoke();
+//                    m_WheelEffects[i].EmitTyreSmoke();
 
                     // avoiding all four tires screeching at the same time
                     // if they do it can lead to some strange audio artefacts
