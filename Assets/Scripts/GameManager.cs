50e3b90be5a5d9e41a4277e4e6d95b2912ff4268
Francisco Baio Dias <xicombd@gmail.com>
2016-02-05 00:22:41 +0000

    Spawn cops on mouse click

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,7 @@ public class GameManager : MonoBehaviour
 	public GameObject CopPrefab;         
 	public GameObject Player;         
 	public float spawnInterval;
+	public Camera CopCam;
 
 	private List<GameObject> Cops = new List<GameObject> ();
 
@@ -19,12 +20,11 @@ public class GameManager : MonoBehaviour
 	}
 
 
-	private void SpawnCop()
+	private void SpawnCop(Vector3 position)
 	{
-		Vector3 spawnPosition = Player.GetComponent<Rigidbody> ().position;
-		spawnPosition.y += 10;
+		position.y = 1;
 
-		GameObject cop = Instantiate(CopPrefab, spawnPosition, Player.GetComponent<Rigidbody>().rotation) as GameObject;
+		GameObject cop = Instantiate(CopPrefab, position, Player.GetComponent<Rigidbody>().rotation) as GameObject;
 
 		cop.GetComponent<CarAIControl>().SetTarget(Player.transform);
 
@@ -34,7 +34,17 @@ public class GameManager : MonoBehaviour
 	private void Update () {
 		if (Input.GetKeyDown (KeyCode.P)) {
 			print ("space key was pressed");
-			SpawnCop ();
+			SpawnCop (Player.GetComponent<Rigidbody> ().position);
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Vector3 pos = Input.mousePosition;
+			pos.z = 100;
+			pos = CopCam.ScreenToWorldPoint(pos);
+
+			SpawnCop (pos);
+
 		}
 	}
 }
