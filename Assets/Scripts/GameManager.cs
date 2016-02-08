6dd2ba00edbab4168d267cba0c0e76f3c7871996
Francisco Baio Dias <xicombd@gmail.com>
2016-02-08 18:49:59 +0000

    Toggle between cameras

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,11 +11,14 @@ public class GameManager : MonoBehaviour
 	public GameObject Player;         
 	public float spawnInterval;
 	public Camera CopCam;
+	public int Layout = 0;
 
+	private Camera PlayerCam;
 	private List<GameObject> Cops = new List<GameObject> ();
 
 	private void Start()
 	{
+		PlayerCam = Player.GetComponentInChildren<Camera> ();
 //		InvokeRepeating ("SpawnCop", 0, spawnInterval);
 	}
 
@@ -37,14 +40,41 @@ public class GameManager : MonoBehaviour
 			SpawnCop (Player.GetComponent<Rigidbody> ().position);
 		}
 
-		if (Input.GetMouseButtonDown(0))
-		{
+		if (Input.GetMouseButtonDown(0)) {
 			Vector3 pos = Input.mousePosition;
 			pos.z = 100;
 			pos = CopCam.ScreenToWorldPoint(pos);
 
 			SpawnCop (pos);
+		}
 
+		UpdateCamera ();
+	}
+
+	private void UpdateCamera () {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			Layout = 0;
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			Layout = 1;
+		}
+		else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			Layout = 2;
+		}
+
+		switch (Layout) {
+		case 0:
+			PlayerCam.rect = new Rect(0, 0, 1, 1);
+			CopCam.rect = new Rect(0, 0, 0, 0);
+			break;
+		case 1:
+			PlayerCam.rect = new Rect(0, 0, 0, 0);
+			CopCam.rect = new Rect(0, 0, 1, 1);
+			break;
+		case 2:
+			PlayerCam.rect = new Rect(0, 0, 0.5f, 1);
+			CopCam.rect = new Rect(0.5f, 0, 0.5f, 1);
+			break;
 		}
 	}
 }
