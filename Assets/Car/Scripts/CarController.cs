09795888e1dbd663c6a46b2e4de37a85bdd20ef1
Raicuparta <raicuparta@gmail.com>
2016-02-08 15:50:54 +0000

    car stuff

--- Assets/Car/Scripts/CarController.cs
+++ Assets/Car/Scripts/CarController.cs
@@ -4,12 +4,16 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class CarController : MonoBehaviour {
     public bool playerControlled = false;
+    public float Speed = 20;
+    public float SidewaysCompensation = 30;
+    public float SuspensionStrength = 5;
+    public float TurningSpeed = 2;
 
-    private Rigidbody body;
+    private Rigidbody Body;
 
 	// Use this for initialization
 	void Start () {
-         body = GetComponent< Rigidbody > ();
+         Body = GetComponent< Rigidbody > ();
     }
 	
 	// Update is called once per frame
@@ -17,17 +21,35 @@ public class CarController : MonoBehaviour {
         float h = CrossPlatformInputManager.GetAxis("Horizontal");
         float v = CrossPlatformInputManager.GetAxis("Vertical");
 
-       Move(h, v, v);
+       Move(h, v);
     }
 
-    void Move (float steering, float accel, float footbrake) {
+    void Move (float steering, float accel) {
 
-        Vector3 velocity = body.velocity;
-        float sidewaysVelocity = transform.InverseTransformDirection(body.velocity).z;
+        Vector3 velocity = Body.velocity;
+        float sidewaysVelocity = transform.InverseTransformDirection(Body.velocity).z;
 
-        body.AddRelativeForce(accel * 10, 0, -sidewaysVelocity * 30);
-        body.AddRelativeForce(accel * 10, 0, 0);
-        body.AddRelativeTorque(0, steering, 0);
+        //Body.AddForceAtPosition();
+        Body.AddRelativeForce(accel * Speed, 0, -sidewaysVelocity * SidewaysCompensation);
+        Body.AddRelativeTorque(0, steering * TurningSpeed, 0);
 
+        Suspension(Body.position + transform.rotation * new Vector3(1, 0, 1));
+        Suspension(Body.position + transform.rotation * new Vector3(-1, 0, 1));
+        Suspension(Body.position + transform.rotation * new Vector3(1, 0, -1));
+        Suspension(Body.position + transform.rotation * new Vector3(-1, 0, -1));
+    }
+
+
+    void Suspension (Vector3 origin) {
+        Vector3 direction = new Vector3(0, -1, 0);
+        RaycastHit info = new RaycastHit();
+        float maxDistance = 1;
+
+        Physics.Raycast(origin, direction, out info, maxDistance);
+        Debug.Log(info.distance);
+        Vector3 push = new Vector3(0, SuspensionStrength / info.distance, 0);
+
+        //Body.AddForce(0, push, 0);
+        Body.AddForceAtPosition(push, origin);
     }
 }
