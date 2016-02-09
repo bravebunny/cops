8b9148c94a2f71a5f14e4e84c5ce24de16c85bbf
Raicuparta <raicuparta@gmail.com>
2016-02-09 19:11:00 +0000

    car model actually

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Car;
+
+    private Camera CarCamera;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+        CarCamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        offset = transform.position - Car.transform.position;
+        Debug.Log(Car.transform.position);
+
+    }
+
+    void LateUpdate () {
+        //transform.Translate(transform.position - Car.transform.position);
+        Vector3 newPosition = CarCamera.transform.position;
+        Debug.Log(Car.transform.position);
+        CarCamera.transform.position = Car.transform.position;
+    }
+}
