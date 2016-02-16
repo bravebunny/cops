db00267aadedfb36714605050ef034814eee182f
Francisco Baio Dias <xicombd@gmail.com>
2016-02-16 02:37:47 +0000

    Distinguish player types

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,36 +6,52 @@ using System.Collections.Generic;
 using UnityEngine.Networking;
 
 public class GameManager : NetworkBehaviour {
-    public GameObject CopPrefab;
-    public GameObject Car;
-    public Camera CarCamera;
-    public Camera CopCamera;
-    public int Layout = 0;
+    public static GameObject CopPrefab;
+    public static GameObject Car;
+    public static Camera CarCamera;
+    public static Camera CopCamera;
+    public static int Layout = 0;
+    public static int Round = 0;
 
+    static public List<NetworkPlayer> Players = new List<NetworkPlayer>();
+    static public List<GameObject> Cops = new List<GameObject> ();
     static public GameManager sInstance = null;
 
-    private List<GameObject> Cops = new List<GameObject> ();
     protected bool _running = true;
 
     void Awake()
     {
         sInstance = this;
+
+        CarCamera = GameObject.Find("CarCamera").GetComponent<Camera>();
+        CopCamera = GameObject.Find("CopCamera").GetComponent<Camera>();
     }
 
     private void Start() {
         Physics.gravity = new Vector3(0, -30.0f, 0);
     }
 
+    public static int RegisterPlayer(NetworkPlayer player) {
+        Players.Add(player);
 
-    private void SpawnCop(Vector3 position) {
+        if (Players.Count == 2) {
+            SetPlayerTypes();
+        }
+
+        return Players.IndexOf(player);
+    }
+
+
+    public static GameObject SpawnCop(Vector3 position) {
         position.y = 2;
 
         GameObject cop = Instantiate(CopPrefab, position, Car.GetComponent<Rigidbody>().rotation) as GameObject;
 
         cop.GetComponent<CarAIController>().SetTarget(Car.transform);
 
-        NetworkServer.Spawn(cop);
         Cops.Add (cop);
+
+        return cop;
     }
 
     private void Update () {
@@ -43,28 +59,39 @@ public class GameManager : NetworkBehaviour {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
-        if (Layout != 0 && Input.GetMouseButtonDown(0)) {
-            Vector3 pos = Input.mousePosition;
-            pos.z = 100;
-            pos = CopCamera.ScreenToWorldPoint(pos);
+//        if (Layout != 0 && Input.GetMouseButtonDown(0)) {
+//            Vector3 pos = Input.mousePosition;
+//            pos.z = 100;
+//            pos = CopCamera.ScreenToWorldPoint(pos);
+//
+//            SpawnCop (pos);
+//        }
 
-            SpawnCop (pos);
-        }
-
-        UpdateCamera ();
+//        if (CarCamera != null && CopCamera != null)
+//            UpdateCamera ();
     }
 
-    private void UpdateCamera () {
-        if (Input.GetKeyDown (KeyCode.Alpha1)) {
+    public static void SetLayoutByPlayerIndex (int index) {
+        string type = GetPlayerType(index);
+        if (type == "CAR")
             Layout = 0;
-        }
-        else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+        else
             Layout = 1;
-        }
-        else if (Input.GetKeyDown (KeyCode.Alpha3)) {
-            Layout = 2;
-        }
 
+        UpdateCamera();
+    }
+
+    private static void UpdateCamera () {
+//        if (Input.GetKeyDown (KeyCode.Alpha1)) {
+//            Layout = 0;
+//        }
+//        else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+//            Layout = 1;
+//        }
+//        else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+//            Layout = 2;
+//        }
+//
         switch (Layout) {
             case 0:
                 CarCamera.rect = new Rect(0, 0, 1, 1);
@@ -79,5 +106,36 @@ public class GameManager : NetworkBehaviour {
                 CopCamera.rect = new Rect(0.5f, 0, 0.5f, 1);
                 break;
         }
+    }
+
+    public static string GetPlayerType (int index) {
+        if (Round % 2 == 1) {
+            if (index % 2 == 0)
+                return "COP";
+            else
+                return "CAR";
+        } else {
+            if (index % 2 == 0)
+                return "CAR";
+            else
+                return "COP";
+        }
+    }
+
+    private static void SetPlayerTypes () {
+        Debug.Log("SetPlayerTypes " + Players.Count.ToString());
+
+        for(int i = 0; i < Players.Count; ++i) {
+            string type = GetPlayerType(i);
+            Players[i].SetType(type);
+
+            if (type == "CAR") {
+                Debug.Log("Get Car");
+                Car = Players[i].gameObject;
+            }
+        }
+        Debug.Log(Car);
+
+        CarCamera.GetComponent<CameraController>().SetTarget(Car.transform);
     }
 }
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(NetworkTransform))]
+[RequireComponent(typeof(Rigidbody))]
+public class NetworkPlayer : NetworkBehaviour {
+    public string Type = null;
+    public int Index;
+
+    protected Rigidbody _rigidbody;
+
+    protected bool canControl = true;
+    protected CarController _carControlller; // the car controller we want to use
+
+    float steering;
+    float accel;
+
+    void Awake() {
+        Debug.Log("NetworkPlayer Awake");
+        //register the player in the gamemanager, that will allow to loop on it.
+        Index = GameManager.RegisterPlayer(this);
+
+        _rigidbody = GetComponent<Rigidbody>();
+        _carControlller = GetComponent<CarController>();
+    }
+        
+    // Use this for initialization
+    void Start () {
+        if (Type != null) {
+            EnableCar(Type == "CAR");
+        }
+
+        if (isLocalPlayer)
+            GameManager.SetLayoutByPlayerIndex(Index);
+    }
+
+    [ClientCallback]
+    void Update() {
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        if (Type == "CAR") {
+            steering = CrossPlatformInputManager.GetAxis("Horizontal");
+            accel = CrossPlatformInputManager.GetAxis("Vertical");
+        } else {
+            if (GameManager.Layout != 0 && Input.GetMouseButtonDown(0)) {
+                Vector3 pos = Input.mousePosition;
+                pos.z = 100;
+                pos = GameManager.CopCamera.ScreenToWorldPoint(pos);
+
+                GameManager.SpawnCop (pos);
+
+                // we call a Command, that will be executed only on server, to spawn a new bullet
+                // we pass the position&forward to be sure to shoot from the right one (server can lag one frame behind)
+                // CmdSpawnCop(pos, transform.forward, _rigidbody.velocity);
+            }
+        }
+    }
+
+
+    [ClientCallback]
+    void FixedUpdate() {
+        if (!isLocalPlayer)
+            return;
+
+        if (Type == "CAR") {
+            _carControlller.Move(steering, accel);
+        }
+    }
+
+    public void SpawnCop(Vector3 position) {
+        GameObject cop = GameManager.SpawnCop(position);
+
+        //NetworkServer.SpawnWithClientAuthority(bullet, connectionToClient);
+    }
+
+    [Command]
+    public void CmdSpawnCop(Vector3 position) {
+        if (!isClient) // avoid to create bullet twice (here & in Rpc call) on hosting client
+            SpawnCop(position);
+
+        RpcSpawnCop(position);
+    }
+
+    [ClientRpc]
+    public void RpcSpawnCop(Vector3 position) {
+        SpawnCop(position);
+    }
+
+
+    // We can't disable the whole object, as it would impair synchronisation/communication
+    // So disabling mean disabling collider & renderer only
+    public void EnableCar(bool enable) {
+        Debug.Log("EnableCar " + Type + " " + enable.ToString());
+        GetComponent<Collider>().enabled = isServer && enable;
+        GetComponent<NetworkTransform>().enabled = enable;
+        GetComponent<CarController>().enabled = enable;
+        GetComponent<Rigidbody>().useGravity = enable;
+
+        canControl = enable;
+
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>()) {
+            renderer.enabled = enable;
+        }
+
+    }
+
+
+    public void SetType (string newType) {
+        Type = newType;
+
+        Debug.Log("SetType " + newType + " " + Type);
+
+        EnableCar(Type == "CAR");
+    }
+
+    void OnDestroy()
+    {
+        GameManager.Players.Remove(this);
+    }
+
+
+}
