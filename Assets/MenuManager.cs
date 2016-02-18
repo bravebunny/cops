d785e97645fa703af63aed9dfef514e20fb2204b
Francisco Baio Dias <xicombd@gmail.com>
2016-02-18 12:53:44 +0000

    Add menu scene

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadLocalGame () {
+        Application.LoadLevel("City");
+    }
+
+    public void LoadNetworkGame () {
+        Application.LoadLevel("NetworkLobby");
+    }
+}
