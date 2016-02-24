2ecf34e64bc1eabc46fbf99c60ec8bad7595c9da
Raicuparta <raicuparta@gmail.com>
2016-02-24 15:57:21 +0000

    prevent jumps into space

--- Assets/Car/Scripts/CarController.cs
+++ Assets/Car/Scripts/CarController.cs
@@ -121,7 +121,8 @@ public class CarController : MonoBehaviour {
             float wheelHeight = 0.25f;
             wheel.position = new Vector3(info.point.x, info.point.y + wheelHeight, info.point.z);
             float strength = SuspensionStrength / (info.distance / SuspensionHeight) - SuspensionStrength;
-            //Vector3 push = transform.rotation * new Vector3(0, strength, 0);
+            strength = Mathf.Min(strength, 30);
+            Debug.Log(strength);
             Vector3 push = transform.up * strength;
             Body.AddForceAtPosition(push, origin);
         } else {
