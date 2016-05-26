76ea8ac446749579ac8f0e52721d79498a5a6aba
Raicuparta <raicuparta@gmail.com>
2016-05-26 13:15:20 +0100

    cleanup

--- Assets/Scripts/TiledLoader.cs
+++ Assets/Scripts/TiledLoader.cs
@@ -40,39 +40,5 @@ public class TiledLoader : MonoBehaviour {
                 Instantiate(obj, position, rotation);
             }
         }
-
-        /*JSONObject j = new JSONObject(Map.ToString());
-        accessData(j);*/
-    }
-
-    //access data (and print it)
-    void accessData(JSONObject obj) {
-        switch (obj.type) {
-            case JSONObject.Type.OBJECT:
-                for (int i = 0; i < obj.list.Count; i++) {
-                    string key = (string)obj.keys[i];
-                    JSONObject j = (JSONObject)obj.list[i];
-                    Debug.Log(key);
-                    accessData(j);
-                }
-                break;
-            case JSONObject.Type.ARRAY:
-                foreach (JSONObject j in obj.list) {
-                    accessData(j);
-                }
-                break;
-            case JSONObject.Type.STRING:
-                Debug.Log(obj.str);
-                break;
-            case JSONObject.Type.NUMBER:
-                Debug.Log(obj.n);
-                break;
-            case JSONObject.Type.BOOL:
-                Debug.Log(obj.b);
-                break;
-            case JSONObject.Type.NULL:
-                Debug.Log("NULL");
-                break;
-        }
     }
 }
