3d21c2a9e94c75491647994d89657d0ad1d56e35
Raicuparta <raicuparta@gmail.com>
2016-09-03 19:39:55 +0100

    auto generated buildings: varying height, roofs

--- Assets/BuildingGenerator.cs
+++ Assets/BuildingGenerator.cs
@@ -3,24 +3,44 @@ using System.Collections;
 
 public class BuildingGenerator : MonoBehaviour {
     public GameObject[] Walls;
-    public float Size;
+    public GameObject[] Roofs;
+    [Range(1,50)]
+    public int MaxFloor = 4;
+    public float FloorHeight = 2;
+    float Scale = 2;
 
+    Vector3[] Corners = {
+        Vector3.left + Vector3.forward,
+        Vector3.right + Vector3.forward,
+        Vector3.right + Vector3.back,
+        Vector3.left + Vector3.back,
+    };
 
 	void Start () {
-        Vector3[] positions = {
-            Vector3.left + Vector3.forward,
-            Vector3.right + Vector3.forward,
-            Vector3.right + Vector3.back,
-            Vector3.left + Vector3.back,
-        };
+        int height = Random.Range(1, MaxFloor);
 
+        for (int floor = 0; floor < height; floor++) {
+            AddWalls(floor);
+        }
+        AddCeilling(height);
+	}
+
+    void AddCeilling(int height) {
+        int index = Random.Range(0, Roofs.Length);
+        GameObject roof = Roofs[index];
+        Vector3 position = transform.position + Vector3.up * height * FloorHeight;
+        Instantiate(roof, position, Quaternion.identity);
+    }
+
+    void AddWalls(int floor) {
         float angle = 0;
-        foreach (Vector3 pos in positions) {
+        foreach (Vector3 corner in Corners) {
+            Vector3 pos = corner * Scale + Vector3.up * floor * FloorHeight;
             AddWall(pos, angle);
             angle += 90;
             AddWall(pos, angle);
         }
-	}
+    }
 
     void AddWall(Vector3 pos, float angle) {
         int index = Random.Range(0, Walls.Length);
