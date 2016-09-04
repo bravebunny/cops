24b8dae1e10a2ce0cd4f166bffda690b76cee3e7
Raicuparta <raicuparta@gmail.com>
2016-09-04 20:32:37 +0100

    split meshes to avoid surpassing the vertex limit

--- Assets/CombineChildren.cs
+++ Assets/CombineChildren.cs
@@ -10,14 +10,18 @@ public class CombineChildren : MonoBehaviour {
         Dictionary<string, List<CombineInstance>> combines = new Dictionary<string, List<CombineInstance>>();
         Dictionary<string, Material> namedMaterials = new Dictionary<string, Material>();
         Dictionary<string, int> vertexCount = new Dictionary<string, int>();
+        Dictionary<string, int> matIndexes = new Dictionary<string, int>();
         MeshRenderer[] meshRenderers = GetComponentsInChildren<MeshRenderer>();
         List<GameObject> toDestroy = new List<GameObject>();
 
         foreach (var meshRenderer in meshRenderers) {
             foreach (var material in meshRenderer.sharedMaterials)
-                if (material != null && !combines.ContainsKey(material.name)) {
-                    combines.Add(material.name, new List<CombineInstance>());
-                    namedMaterials.Add(material.name, material);
+                if (material != null && !combines.ContainsKey(material.name + "0")) {
+                    string name = material.name;
+                    combines.Add(name + "0", new List<CombineInstance>());
+                    vertexCount.Add(name, 0);
+                    matIndexes.Add(name, 0);
+                    namedMaterials.Add(name, material);
                 }
         }
 
@@ -40,28 +44,33 @@ public class CombineChildren : MonoBehaviour {
 
             // keep count of the vertex count for each material to avoid going over the vertex limit
             string name = filterRenderer.sharedMaterial.name;
-            if (!vertexCount.ContainsKey(name)) vertexCount.Add(name, 0);
             vertexCount[name] += filter.sharedMesh.vertexCount;
-            if (vertexCount[name] >= UInt16.MaxValue) continue;
-            combines[name].Add(ci);
+            if (vertexCount[name] >= UInt16.MaxValue) {
+                matIndexes[name]++;
+                vertexCount[name] = filter.sharedMesh.vertexCount;
+                combines.Add(name + matIndexes[name], new List<CombineInstance>());
+            };
+            combines[name + matIndexes[name]].Add(ci);
 
             DestroyImmediate(filterRenderer);
         }
 
         foreach (Material m in namedMaterials.Values) {
-            var go = new GameObject(m.name);
-            go.transform.parent = transform;
-            go.transform.localPosition = Vector3.zero;
-            go.transform.localRotation = Quaternion.identity;
-            go.transform.localScale = Vector3.one;
+            for (int i = 0; i <= matIndexes[m.name]; i++) {
+                var go = new GameObject(m.name);
+                go.transform.parent = transform;
+                go.transform.localPosition = Vector3.zero;
+                go.transform.localRotation = Quaternion.identity;
+                go.transform.localScale = Vector3.one;
 
-            var filter = go.AddComponent<MeshFilter>();
-            filter.mesh.CombineMeshes(combines[m.name].ToArray(), true, true);
+                var filter = go.AddComponent<MeshFilter>();
+                filter.mesh.CombineMeshes(combines[m.name + i].ToArray(), true, true);
 
-            var arenderer = go.AddComponent<MeshRenderer>();
-            arenderer.material = m;
+                var arenderer = go.AddComponent<MeshRenderer>();
+                arenderer.material = m;
 
-            go.AddComponent<MeshCollider>();
+                go.AddComponent<MeshCollider>();
+            }
         }
 
         foreach (GameObject g in toDestroy) {
