41f983e78fdd29c6be821f8f1f2fafbd973a9cb4
Raicuparta <raicuparta@gmail.com>
2016-09-06 10:02:06 +0100

    police lights

---
+++ Assets/Scripts/PoliceLights.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoliceLights : MonoBehaviour {
+    public Light Red;
+    public Light Blue;
+    public float Rate = 1;
+
+	void Start () {
+        Red.enabled = false;
+        Blue.enabled = true;
+
+        // Wait a random amount of time before starting,
+        // so all the cops' lights don't blink at the same time
+        float wait = Random.Range(0, Rate);
+        InvokeRepeating("Switch", wait, Rate);
+	}
+	
+	void Switch() {
+        Red.enabled = !Red.enabled;
+        Blue.enabled = !Blue.enabled;
+    }
+}
