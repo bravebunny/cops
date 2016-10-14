08d41cf4364aae3693d45224f33572d1aca9e72b
Raicuparta <raicuparta@gmail.com>
2016-10-14 14:34:33 +0200

    train iron movement

---
+++ Assets/RotateAround.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAround : MonoBehaviour {
+    public Transform Pivot;
+    public float Speed = 10;
+    Quaternion InitialRotation;
+
+    void Start() {
+        InitialRotation = transform.localRotation;
+    }
+	
+	void FixedUpdate() {
+        transform.RotateAround(Pivot.position, transform.parent.forward, Speed);
+        transform.localRotation = InitialRotation;
+    }
+}
