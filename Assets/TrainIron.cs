092a3ad43cf5b019fb7d0823d27af6799b4df12b
Raicuparta <raicuparta@gmail.com>
2016-10-17 19:30:44 +0200

    flexible train iron

---
+++ Assets/TrainIron.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainIron : MonoBehaviour {
+    public Transform Pivot;
+    float InitialLength;
+
+    void Start () {
+        InitialLength = Vector3.Distance(transform.position, Pivot.position);
+	}
+	
+	void Update () {
+        float scale = Vector3.Distance(transform.position, Pivot.position) / InitialLength;
+        transform.localScale = new Vector3(1, 1, scale);
+
+        // vector that points from this object to the pivot
+        Vector3 toPivot = Pivot.position - transform.position;
+        // horizontal vector used to measure the current angle
+        Vector3 horizontal = Vector3.ProjectOnPlane(-transform.forward, Vector3.up);
+        float angle = Vector3.Angle(toPivot, horizontal);
+        if (toPivot.y < horizontal.y) angle *= -1;
+        Vector3 euler = transform.eulerAngles;
+        transform.eulerAngles = new Vector3(angle, euler.y, euler.z);
+    }
+}
