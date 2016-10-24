915b5452ffd2c6bd31bad07ab41a3abb7a9fbd2f
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2016-10-24 19:03:53 +0200

    Corn

---
+++ Assets/Corn.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corn : MonoBehaviour {
+
+    bool collisionOn;
+    bool checkTrigger = true;
+    Vector3 heading;
+    Vector3 direction;
+    public float speed = 0.01F;
+
+    // Use this for initialization
+    void Start () {
+        collisionOn = false;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+	    if (collisionOn) {
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, direction, speed, 0.0F);
+            transform.rotation = Quaternion.LookRotation(newDir.normalized);
+        }
+    }
+
+    void OnTriggerEnter(Collider coll) {
+        if (coll.tag == "Player" && checkTrigger) {
+            checkTrigger = false;
+            heading = coll.transform.position - transform.position;
+            direction = heading.normalized;
+            collisionOn = true;
+        }
+       
+    }
+}
