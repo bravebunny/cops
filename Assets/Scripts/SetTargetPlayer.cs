9dacb2a45996aaca688c6a03fefac67002f4fd53
Raicuparta <raicuparta@gmail.com>
2016-10-24 21:45:17 +0200

    set target player fix

--- Assets/Scripts/SetTargetPlayer.cs
+++ Assets/Scripts/SetTargetPlayer.cs
@@ -3,6 +3,6 @@ using System.Collections;
 
 public class SetTargetPlayer : MonoBehaviour {
 	void Start () {
-        GetComponent<AIPath>().target = GameManager.Player.transform;
+        GetComponent<CarAIController>().Target = GameManager.Player.transform;
 	}
 }
