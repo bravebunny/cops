651a4847c16688e6d593bd9006963627c833cab7
Raicuparta <raicuparta@gmail.com>
2016-10-24 22:21:33 +0200

    corn destruction

---
+++ Assets/Scripts/Corn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Corn : MonoBehaviour {
+    bool Fall;
+    Vector3 Direction;
+    Rigidbody Body;
+    BoxCollider Collider;
+    public float Speed = 0.01F;
+	
+    void Start() {
+        Body = GetComponent<Rigidbody>();
+        Collider = GetComponent<BoxCollider>();
+    }
+
+	void FixedUpdate () {
+        if (!Fall) return;
+        Vector3 newDir = Vector3.RotateTowards(transform.up, Direction, Speed, 0.0F);
+        transform.up = newDir;
+    }
+
+    void OnTriggerEnter(Collider coll) {
+        Collider.enabled = false;
+        //Body.isKinematic = false;
+        Fall = true;
+        Direction = (transform.position - coll.transform.position).normalized;
+    }
+}
