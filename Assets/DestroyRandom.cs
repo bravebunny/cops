235da27a862be92f7915868b3b745ebfc388e9a5
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2016-10-27 21:32:51 +0200

    smoke from chimney

---
+++ Assets/DestroyRandom.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyRandom : MonoBehaviour {
+
+    public float Probability = 0.5F;
+
+    // Use this for initialization
+    void Start() {
+        if (Random.Range(0.0F, 1.0F) <= Probability) Destroy(gameObject);
+    }
+}
