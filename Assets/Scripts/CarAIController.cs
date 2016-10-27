f921003b68ab68dfcfd41d344d9004dfdbf6e2c0
Raicuparta <raicuparta@gmail.com>
2016-10-27 17:01:21 +0200

    simpler cop AI

--- Assets/Scripts/CarAIController.cs
+++ Assets/Scripts/CarAIController.cs
@@ -1,56 +1,24 @@
 ﻿using UnityEngine;
-using System.Collections;
-using Pathfinding;
-using Pathfinding.RVO;
-using System.Collections.Generic;
 
 public class CarAIController : MonoBehaviour {
 
     CarController CarController;    // Reference to actual car controller we are controlling
-    Rigidbody Body;
     ExplodeOnImpact ImpactExplosion;
     public Transform Target;               // 'target' the target object to aim for.
-    Vector3 TargetPosition;
-    Seeker AISeeker;
-
-    public float PathFindingInterval = 1;
+    Vector3 Direction;
 
     void Awake() {
         CarController = GetComponent<CarController>();
-        Body = GetComponent<Rigidbody>();
         ImpactExplosion = GetComponent<ExplodeOnImpact>();
-        AISeeker = GetComponent<Seeker>();
     }
 
     void Start() {
-        InvokeRepeating("CalculatePath", Random.Range(0, PathFindingInterval), PathFindingInterval);
         Target = GameManager.Player.transform;
-        TargetPosition = Target.position;
-    }
-
-    void OnEnable() {
-        AISeeker.pathCallback += OnPathComplete;
-        // claim?
-
-    }
-
-    public virtual void OnPathComplete(Path path) {
-        List<Vector3> vPath = path.vectorPath;
-        TargetPosition = vPath[1] - vPath[0];
-        //Debug.Log("complete");
-    }
-
-    void CalculatePath() {
-        AISeeker.StartPath(transform.position, Target.position);
     }
 
     void FixedUpdate() {
-        float steering = Vector3.Dot(transform.right, TargetPosition.normalized);
-        CarController.Move(steering, 1);
-    }
-
-    public void SetTarget(Transform target) {
-        Target = target;
+        Direction = (Target.position - transform.position).normalized;
+        CarController.Move(Vector3.Dot(transform.right, Direction), 1);
     }
 
     public void OnTriggerEnter(Collider col) {
@@ -59,5 +27,4 @@ public class CarAIController : MonoBehaviour {
         ImpactExplosion.Enabled = true;
         CarController.Stabilise = false;
     }
-
 }
