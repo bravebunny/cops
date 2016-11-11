373339d667b108b41b2653a323bcba8443e99061
Raicuparta <raicuparta@gmail.com>
2016-11-11 20:41:22 +0100

    train throws cars upwards

---
+++ Assets/Scripts/DestroyIfDrowned.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyIfDrowned : MonoBehaviour {
+    public float MinY = -10;
+	
+	void Update () {
+        if (transform.position.y < MinY) Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/ThorwOnImpact.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThorwOnImpact : MonoBehaviour {
+    public float ImpulseStrength = 10;
+    float UpMultiplier = 1;
+    float ForwardMultiplier = 0.75f;
+
+	void OnCollisionEnter(Collision col) {
+        Debug.Log("cmon");
+        Rigidbody body = col.transform.GetComponent<Rigidbody>();
+        if (body == null) return;
+        Vector3 up = Vector3.up * UpMultiplier;
+        Vector3 forward = ((transform.position - col.transform.position) + transform.parent.forward).normalized * ForwardMultiplier;
+        Vector3 force = (up + forward) * ImpulseStrength;
+        body.AddForce(force, ForceMode.Impulse);
+    }
+
+    void Update() {
+        Debug.DrawLine(transform.position, transform.position + transform.parent.forward * 10, Color.red, 0, false);
+    }
+}
