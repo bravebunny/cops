33697805bcf318893b27bb061dcfe049250b86f6
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2016-11-11 16:29:43 +0100

    scene mod

--- Assets/Scripts/LookAtDelay.cs
+++ Assets/Scripts/LookAtDelay.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class LookAtDelay : MonoBehaviour {
 
-    public Transform target;
+    private Transform target;
     public float speed = 0.5F;
     public float lookAtDistance = 1.0F;
 
     // Use this for initialization
-    void Start () {}
+    void Start () {
+        target = GameManager.Player.transform;
+    }
 	
 	void Update () {
-        float distance = Vector3.Distance(transform.position, target.transform.position);
+        float distance = Vector3.Distance(transform.position, target.position);
 
         if (distance < lookAtDistance) {
             Vector3 lookPos = target.position - transform.position;
