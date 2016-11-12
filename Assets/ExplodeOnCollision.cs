87b094163a140d085c1c2bd6b85d4ffc7b973cf0
Raicuparta <raicuparta@gmail.com>
2016-11-12 17:56:48 +0100

    map cleanup

---
+++ Assets/ExplodeOnCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodeOnCollision: MonoBehaviour {
+    public float MinImpulse = 30; // minimum impact impulse magnitude to trigger explosion
+    public GameObject Explosion;
+    public bool Enabled = false;
+
+    void OnCollisionEnter(Collision col) {
+        if (!Enabled || col.impulse.magnitude < MinImpulse) return;
+
+        Instantiate(Explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
