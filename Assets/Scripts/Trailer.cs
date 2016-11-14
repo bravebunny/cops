0dc9c695db7415ff39115eb2e7330c750b6ee048
Raicuparta <raicuparta@gmail.com>
2016-11-14 19:38:04 +0100

    trailer

---
+++ Assets/Scripts/Trailer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trailer : MonoBehaviour {
+
+	void Start () {
+        GetComponent<HingeJoint>().connectedBody = GameManager.Player.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
