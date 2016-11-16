795cf5368d6918ce3c3d3ae47763bbac0a567e16
Raicuparta <raicuparta@gmail.com>
2016-11-16 21:15:43 +0100

    skip on water

---
+++ Assets/Scripts/SkipOnWater.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkipOnWater : MonoBehaviour {
+    public float MinY = -4;
+    public float MinVelocity = 10;
+    public float ImpulseForce = 10;
+    public float Resistance = 5;
+    public Object Splash;
+    Rigidbody Body;
+
+    void Start() {
+        Body = GetComponent<Rigidbody>();
+    }
+
+	void Update () {
+        Vector3 velocity = new Vector3(Body.velocity.x, 0, Body.velocity.z) / 2;
+        if (transform.position.y < MinY && velocity.magnitude > MinVelocity) {
+            Vector3 newPosition = transform.position;
+            newPosition.y = MinY;
+            transform.position = newPosition;
+            Vector3 force = (Vector3.down * ImpulseForce * Body.velocity.y) - (velocity.normalized * Resistance);
+            Vector3 forcePosition = transform.position - velocity.normalized;
+            Body.AddForceAtPosition(force, forcePosition, ForceMode.Impulse);
+            Instantiate(Splash, transform.position, Quaternion.identity);
+        }
+	}
+}
