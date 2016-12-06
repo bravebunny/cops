ceb19ae05fe345cc790de2080a0d0fe335af35ac
Raicuparta <raicuparta@gmail.com>
2016-12-06 23:16:28 +0100

    random map improvements

--- Assets/RandomMap.cs
+++ Assets/RandomMap.cs
@@ -1,90 +1,92 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class RandomMap : MonoBehaviour {
+    public Chunk[] BaseChunks;
     public Chunk[] Chunks;
+    public Chunk Empty;
     public GameObject Loader;
     public int SizeX = 10;
     public int SizeZ = 10;
     public float ChunkSize = 9;
     public float TileSize = 4;
+    Voxel[,] Voxels;
 
-	void Start () {
-        Debug.Log("## first ##");
-        Chunk first = PickFirstChunk();
-        Debug.Log("## next ##");
-        Chunk previous = first;
-        for (int x = 0; x < 5; x++) {
-            previous = PickNextChunk(previous);
-        }
-        /*for (int x = 0; x < SizeX; x++) {
+    private IEnumerator Generate() {
+        WaitForSeconds wait = new WaitForSeconds(0.1f);
+        for (int x = 0; x < SizeX; x++) {
             for (int z = 0; z < SizeZ; z++) {
-                GameObject instance = Instantiate<GameObject>(Loader);
-
-                Vector3 position = new Vector3(x, 0, z) * ChunkSize * TileSize;
-                instance.transform.position = position;
-
-                TiledLoader tl = instance.GetComponent<TiledLoader>();
-                int chunkIndex = Random.Range(0, Chunks.Length);
-                tl.Map = Chunks[chunkIndex];
-                tl.Build();
-
-                int angle = Random.Range(0, 3) * 90;
-                instance.transform.eulerAngles = Vector3.up * angle;
-            }
-        }*/
-    }
-
-    Chunk RandomChunk() {
-        int index = Random.Range(0, Chunks.Length);
-        Chunk chunk = new Chunk(Chunks[index]);
-        chunk.RandomRotation();
-        return chunk;
-    }
-
-    Chunk PickFirstChunk() {
-        Chunk chunk = RandomChunk();
-
-        GameObject instance = Instantiate<GameObject>(Loader);
-        TiledLoader tl = instance.GetComponent<TiledLoader>();
-        tl.Map = chunk.TiledMap;
-        tl.Build();
-
-        int angle = chunk.rotation * 90;
-        instance.transform.eulerAngles = Vector3.up * angle;
-        Debug.Log(chunk.ToString());
-        return chunk;
-    }
-
-    void InstantiateChunk(Chunk chunk, Vector3 euler) {
-        GameObject instance = Instantiate<GameObject>(Loader);
-        TiledLoader tl = instance.GetComponent<TiledLoader>();
-        tl.Map = chunk.TiledMap;
-        tl.Build();
-        instance.transform.position = chunk.position;
-        instance.transform.eulerAngles = euler;
-    }
-
-    Chunk PickNextChunk(Chunk previous) {
-        Chunk chunk = RandomChunk();
-        int rotationCount = 0;
-        while (previous.right != chunk.left) {
-            chunk.RotateRight();
-            Debug.Log("$while" + rotationCount + ": previous(" + previous.ToString() + ") current(" + chunk.ToString() + ")");
-            rotationCount++;
-            if (rotationCount > 4) {
-                Debug.LogError("No possible chunk found");
-                Debug.Log(chunk.ToString());
-                break;
+                CreateChunk(x, z);
+                yield return wait;
             }
         }
-        Debug.Log("$$ after while: previous=" + previous.right + ", current=" + chunk.left);
-        Vector3 euler = Vector3.up * chunk.rotation * 90;
-        chunk.position = previous.position + Vector3.right * ChunkSize * TileSize;
-        InstantiateChunk(chunk, euler);
-        Debug.Log(chunk.ToString());
-        return chunk;
+    }
+
+    void Start () {
+        Chunks = new Chunk[BaseChunks.Length * 4];
+
+        for (int i = 0; i < BaseChunks.Length; i++) {
+            for (int rotation = 0; rotation < 4; rotation++) {
+                Chunk newChunk = new Chunk(BaseChunks[i]);
+                newChunk.SetRotation(rotation);
+                Chunks[i * 4 + rotation] = newChunk;
+            }
+        }
+
+        Voxels = new Voxel[SizeX, SizeZ];
+        float size = ChunkSize * TileSize;
+        for (int x = 0; x < SizeX; x++) {
+            for (int z = 0; z < SizeZ; z++) {
+                Voxels[x, z] = new Voxel(x, z, size);
+            }
+        }
+
+        StartCoroutine(Generate());
+    }
+
+    void CreateChunk(int x, int z) {
+        Voxel current = Voxels[x, z];
+        if (!current.value) {
+            InstantiateChunk(Empty, current.Position);
+            return;
+        }
+
+        bool top = false, right = false, bottom = false, left = false;
+        if (z + 1 < Voxels.GetLength(1)) top = Voxels[x, z + 1].value;
+        if (x + 1 < Voxels.GetLength(0)) right = Voxels[x + 1, z].value;
+        if (z > 0) bottom = Voxels[x, z - 1].value;
+        if (x > 0) left = Voxels[x - 1, z].value;
+        
+        List<Chunk> validChunks = new List<Chunk>();
+        foreach (Chunk chunk in Chunks) {
+            if (chunk.top == top &&
+                chunk.right == right &&
+                chunk.bottom == bottom &&
+                chunk.left == left)
+                InstantiateChunk(chunk, current.Position);
+        }
+    }
+
+    /*
+    Chunk PickChunkX(Voxel current, Voxel x) {
+        List<Chunk> validChunks = new List<Chunk>();
+        foreach (Chunk chunk in Chunks) {
+            if (chunk.left == current.top && chunk.bottom == x.left)
+                validChunks.Add(chunk);
+        }
+        int index = Random.Range(0, validChunks.Count);
+        return validChunks[index];
+    }*/
+
+    void InstantiateChunk(Chunk chunk, Vector3 position) {
+        GameObject instance = Instantiate<GameObject>(Loader);
+        instance.transform.position = position;
+        TiledLoader tl = instance.GetComponent<TiledLoader>();
+        tl.Map = chunk.TiledMap;
+        tl.Build();
+        tl.transform.eulerAngles = Vector3.up * chunk.rotation * 90;
     }
 
     [System.Serializable]
@@ -95,7 +97,6 @@ public class RandomMap : MonoBehaviour {
         public bool bottom;
         public bool left;
         public int rotation;
-        public Vector3 position;
 
         public Chunk(Chunk chunk) {
             TiledMap = chunk.TiledMap;
@@ -103,29 +104,55 @@ public class RandomMap : MonoBehaviour {
             right = chunk.right;
             bottom = chunk.bottom;
             left = chunk.left;
-            rotation = chunk.rotation;
         }
 
-        public void RandomRotation() {
-            int amount = Random.Range(0, 4);
-            for (int i = 0; i < amount; i++) {
-                RotateRight();
-            }
+        public void SetRotation(int rotation) {
+            for (int i = 0; i < rotation; i++) RotateRight();
         }
 
         public void RotateRight() {
-            Debug.Log("Before rotation: " + ToString());
+            //Debug.Log("Before rotation: " + ToString());
             bool oldTop = top;
             top = left;
             left = bottom;
             bottom = right;
             right = oldTop;
-            rotation--;
-            Debug.Log("After rotation: " + ToString());
+            rotation++;
+            //Debug.Log("After rotation: " + ToString());
         }
 
         public string ToString() {
             return top + "," + right + "," + bottom + "," + left;
+        }
+    }
+
+    public class Voxel {
+        public bool value;
+        public Vector3 Position;
+        public Vector3 ZCorner;
+        public Vector3 XCorner;
+
+        public Voxel (int x, int z, float size) {
+            Position = new Vector3(x * size, 0, z * size);
+            ZCorner = Position + Vector3.forward * size;
+            XCorner = Position + Vector3.right * size;
+            value = RandomBool();
+        }
+
+        bool RandomBool() {
+            return Random.value < 0.5;
+        }
+    }
+
+    void OnDrawGizmos() {
+        if (Voxels == null) return;
+        foreach (Voxel v in Voxels) {
+            Gizmos.color = v.value ? Color.blue : Color.black;
+            Gizmos.DrawCube(v.Position, Vector3.one * 10);
+            /*Gizmos.color = v.left ? Color.blue : Color.black;
+            Gizmos.DrawLine(v.Position, v.ZCorner);
+            Gizmos.color = v.top ? Color.blue : Color.black;
+            Gizmos.DrawLine(v.Position, v.XCorner);*/
         }
     }
 }
