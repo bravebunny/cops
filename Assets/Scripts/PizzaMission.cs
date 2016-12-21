7cbd5552c5bd955a6b8583749788901b4dc3240f
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2016-12-21 13:23:06 +0100

    pzza cenas

--- Assets/Scripts/PizzaMission.cs
+++ Assets/Scripts/PizzaMission.cs
@@ -4,14 +4,14 @@ using System;
 
 public class PizzaMission : MissionsAbstract {
 
-    //public GameObject Target;
+    public GameObject Target;
     MissionManager MM;
 
     public override void InitiateMission(MissionManager missionManager) {
         Debug.Log("Initiate Pizza Mission press space to skip");
         MM = missionManager;
-        MM.TargetScript.SetMission(this, false);
-        //MM.TargetScript.GetComponent<Targets>().NewTarget(Target);
+        MM.TargetScript.SetMission(this, true);
+        MM.TargetScript.GetComponent<Targets>().NewTarget(Target);
     }
 
     public override void EndMission() {
