c271d79a985b9fc8b9efe7a045c7c2e8651420be
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2016-12-26 19:39:40 +0100

    check cargo on van

--- Assets/Scripts/SpawnCargo.cs
+++ Assets/Scripts/SpawnCargo.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class SpawnCargo : MonoBehaviour {
 
     public GameObject CargoVan;
-	
+    public bool CargonOnVan;
+
     public void Spawn(GameObject Cargo) {
-       GameObject cargo = Instantiate<GameObject>(Cargo);
-       cargo.transform.position = CargoVan.transform.position;
+        GameObject cargo = Instantiate<GameObject>(Cargo);
+        cargo.transform.position = CargoVan.transform.position;
+        CargonOnVan = true;
+    }
+
+    void OnTriggerExit(Collider collider) {
+        if (collider.tag == "Cargo") {
+            Debug.Log("cargo off");
+            CargonOnVan = false;
+        }
     }
 }
