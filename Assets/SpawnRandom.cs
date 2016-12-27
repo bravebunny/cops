34b5e2b7fb8aeb83eeccf564c209b54ba70f117d
Raicuparta <raicuparta@gmail.com>
2016-12-27 18:35:07 +0100

    chance of spawning firehydrants and trashcans

--- Assets/SpawnRandom.cs
+++ Assets/SpawnRandom.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class SpawnRandom : MonoBehaviour {
     public GameObject[] Objects; // one of these will be spawned
     public Transform[] Positions; // where to spawn objects;
-
+    public bool RandomAngle = true;
+    public float Chance = 0.5f;
     public float height = 4.5f; // height that must be empty for spawning
 
     void Start() {
+        float random = Random.value;
+        if (random > Chance) return;
+
         // check if there is nothing on top of this tile
         Vector3 origin = transform.position + Vector3.up * height;
         bool hit = Physics.Raycast(origin, Vector3.down, height - 0.1f);
@@ -24,8 +28,11 @@ public class SpawnRandom : MonoBehaviour {
         GameObject instance = Instantiate(Objects[index]);
         instance.transform.position = position.position;
         instance.transform.parent = transform;
-        float angle = Random.Range(0, 360);
-        instance.transform.eulerAngles = Vector3.up * angle;
+
+        if (RandomAngle) {
+            float angle = Random.Range(0, 360);
+            instance.transform.eulerAngles = Vector3.up * angle;
+        } else instance.transform.eulerAngles = position.eulerAngles;
 	}
 
     void Clear() {
