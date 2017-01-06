43b778d686b84a15d1adacc4cb3a1adac7d4deb5
Raicuparta <raicuparta@gmail.com>
2017-01-06 20:11:46 +0100

    cops barely ever get stuck anymore

--- Assets/Scripts/CarAIController.cs
+++ Assets/Scripts/CarAIController.cs
@@ -5,6 +5,9 @@ public class CarAIController : MonoBehaviour {
     CarController CarController;    // Reference to actual car controller we are controlling
     ExplodeOnImpact ImpactExplosion;
     public Transform Target;               // 'target' the target object to aim for.
+    public float LookDistance = 5; // distance to look ahead for obstacles
+    public float AvoidDistance = 1; // distance to add to the new direction to avoid obstacles
+    public LayerMask ObstacleLayers;
     Vector3 Direction;
 
     void Awake() {
@@ -19,7 +22,28 @@ public class CarAIController : MonoBehaviour {
 
     void FixedUpdate() {
         Direction = (Target.position - transform.position).normalized;
+
+        RaycastHit info;
+        bool hit = Physics.Raycast(transform.position, Direction, out info, LookDistance, ObstacleLayers);
+        Debug.DrawLine(transform.position, transform.position + Direction * LookDistance, Color.blue, -1, false);
+        if (hit) {
+            Direction += Vector3.ProjectOnPlane(transform.right, Vector3.up) * AngleDir(transform.forward, info.normal, Vector3.up);
+            Direction.Normalize();
+            Debug.DrawLine(transform.position, transform.position + Direction, Color.red, -1, false);
+        }
+
         CarController.Move(Vector3.Dot(transform.right, Direction), 1);
+    }
+
+    float AngleDir(Vector3 fwd, Vector3 targetDir, Vector3 up) {
+        Vector3 perp = Vector3.Cross(fwd, targetDir);
+        float dir = Vector3.Dot(perp, up);
+
+        if (dir > 0f) {
+            return 1f;
+        } else {
+            return -1f;
+        }
     }
 
     public void OnTriggerEnter(Collider col) {
