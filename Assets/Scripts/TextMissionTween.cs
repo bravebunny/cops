f3999bf390261f116b68d6ef78b55eb0e5417951
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2017-01-09 15:34:34 +0100

    overlay effect on mission text

--- Assets/Scripts/TextMissionTween.cs
+++ Assets/Scripts/TextMissionTween.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
+using UnityStandardAssets.ImageEffects;
 
 public class TextMissionTween : MonoBehaviour {
     public float MidleXPosition;
@@ -8,16 +8,24 @@ public class TextMissionTween : MonoBehaviour {
     public float TextMovingTime;
     public float TextSlowMoTime;
 
+    public Camera MainCamera;
+
     public void StartTextTween() {
         gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-Screen.width, YPosition);
         LeanTween.moveX(gameObject.GetComponent<RectTransform>(), MidleXPosition - MidleSlowDistance*0.5F, TextMovingTime).setEase(LeanTweenType.easeInExpo).setOnComplete(MiddleTween);
     }
 
     void MiddleTween() {
+        MainCamera.GetComponent<ScreenOverlay>().enabled = true;
         LeanTween.moveX(gameObject.GetComponent<RectTransform>(), MidleSlowDistance * 0.5F - MidleXPosition, TextSlowMoTime).setEase(LeanTweenType.linear).setOnComplete(EaseOut);
     }
 
     void EaseOut() {
         LeanTween.moveX(gameObject.GetComponent<RectTransform>(), Screen.width, TextMovingTime).setEase(LeanTweenType.easeOutExpo);
+        MainCamera.GetComponent<ScreenOverlay>().enabled = false;
     }
+
+    /*void StopOverlayEffect() {
+
+    }*/
 }
