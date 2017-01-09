929e8ec4d1c33ee92513e2fc5793fa8d4abc66c8
Raicuparta <raicuparta@gmail.com>
2017-01-09 23:41:17 +0100

    simpler police AI script

--- Assets/Scripts/CarAIController.cs
+++ Assets/Scripts/CarAIController.cs
@@ -1,38 +1,47 @@
 ﻿using UnityEngine;
 
 public class CarAIController : MonoBehaviour {
-
-    CarController CarController;    // Reference to actual car controller we are controlling
     ExplodeOnImpact ImpactExplosion;
     public Transform Target;               // 'target' the target object to aim for.
     public float LookDistance = 5; // distance to look ahead for obstacles
     public float AvoidDistance = 1; // distance to add to the new direction to avoid obstacles
+    public float Speed = 100;
+    public float UpdateRate = 0.5f; // interval in seconds to update direction
     public LayerMask ObstacleLayers;
     Vector3 Direction;
+    Vector3 Velocity;
+    Rigidbody Body;
 
     void Awake() {
-        CarController = GetComponent<CarController>();
         ImpactExplosion = GetComponent<ExplodeOnImpact>();
+        Body = GetComponent<Rigidbody>();
     }
 
     void Start() {
         GameManager.CopCount++;
         Target = GameManager.Player.transform;
+        InvokeRepeating("UpdateDirection", Random.Range(0, UpdateRate), UpdateRate);
+    }
+
+    void UpdateDirection() {
+        transform.LookAt(Target);
+        Velocity = transform.forward * Speed;
     }
 
     void FixedUpdate() {
-        Direction = (Target.position - transform.position).normalized;
+        //Direction = (Target.position - transform.position).normalized;
+        Body.AddForce(Velocity);
 
-        RaycastHit info;
+        /*RaycastHit info;
         bool hit = Physics.Raycast(transform.position, Direction, out info, LookDistance, ObstacleLayers);
         Debug.DrawLine(transform.position, transform.position + Direction * LookDistance, Color.blue, -1, false);
         if (hit) {
             Direction += Vector3.ProjectOnPlane(transform.right, Vector3.up) * AngleDir(transform.forward, info.normal, Vector3.up);
             Direction.Normalize();
             Debug.DrawLine(transform.position, transform.position + Direction, Color.red, -1, false);
-        }
+        }*/
 
-        CarController.Move(Vector3.Dot(transform.right, Direction), 1);
+        //CarController.Move(Vector3.Dot(transform.right, Direction), 1);
     }
 
     float AngleDir(Vector3 fwd, Vector3 targetDir, Vector3 up) {
@@ -48,7 +57,7 @@ public class CarAIController : MonoBehaviour {
 
     public void OnTriggerEnter(Collider col) {
         if (!col.CompareTag("FireHydrant")) return;
-        CarController.Stabilise = false;
+        //CarController.Stabilise = false;
         // TODO move this away from here
     }
 
