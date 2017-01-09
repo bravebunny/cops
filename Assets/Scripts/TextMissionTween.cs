cb835915ced18d0eb39e5c65ba45934708cc2546
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2017-01-09 15:01:09 +0100

    mission text using screen width

---
+++ Assets/Scripts/TextMissionTween.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMissionTween : MonoBehaviour {
+    public float MidleXPosition;
+    public float MidleSlowDistance;
+    public float YPosition;
+    public float TextMovingTime;
+    public float TextSlowMoTime;
+
+    public void StartTextTween() {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(-Screen.width, YPosition);
+        LeanTween.moveX(gameObject.GetComponent<RectTransform>(), MidleXPosition - MidleSlowDistance*0.5F, TextMovingTime).setEase(LeanTweenType.easeInExpo).setOnComplete(MiddleTween);
+    }
+
+    void MiddleTween() {
+        LeanTween.moveX(gameObject.GetComponent<RectTransform>(), MidleSlowDistance * 0.5F - MidleXPosition, TextSlowMoTime).setEase(LeanTweenType.linear).setOnComplete(EaseOut);
+    }
+
+    void EaseOut() {
+        LeanTween.moveX(gameObject.GetComponent<RectTransform>(), Screen.width, TextMovingTime).setEase(LeanTweenType.easeOutExpo);
+    }
+}
