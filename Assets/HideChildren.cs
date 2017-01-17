39cd9d58be19c012c6dee805bc32ed28ab2697ad
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2017-01-17 15:35:42 +0100

    Camera hide buildings finale

--- Assets/HideChildren.cs
+++ Assets/HideChildren.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HideChildren : MonoBehaviour {
 
-    Material[] Material;
+    List<Material> Material;
+    List<MeshRenderer> MeshRend;
     bool FadeIn = false;
     bool FadeOut = false;
+    Material LastMaterial;
 
     void Start() {
-        Material = new Material[transform.childCount];
+
+        MeshRend = new List<MeshRenderer>();
+        for (int i = 0; i < transform.childCount; i++) {
+            if (transform.GetChild(i).GetComponent<MeshRenderer>())
+                MeshRend.Add(transform.GetChild(i).GetComponent<MeshRenderer>());
+        }
+
+        Material = new List<Material>();
         for (int i = 0; i < transform.childCount; i++) {
             if (transform.GetChild(i).GetComponent<Renderer>())
-                Material[i] = transform.GetChild(i).GetComponent<Renderer>().material;
+                Material.Add(transform.GetChild(i).GetComponent<Renderer>().material);
         }
+
+        if(Material.Count > 0)
+            LastMaterial = Material[Material.Count - 1];
     }
 
     void OnTriggerEnter(Collider collider) {
@@ -22,15 +35,19 @@ public class HideChildren : MonoBehaviour {
             FadeOut = true;
             foreach (Material mat in Material) {
                 if (mat) {
-                    //child.GetComponent<Renderer>().enabled = false;
-                    mat.SetFloat("_Mode", 4);
+                    mat.SetFloat("_Mode", 3);
                     mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
                     mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
-                    mat.SetInt("_ZWrite", 0);
+                    mat.SetInt("_ZWrite", 1);
                     mat.DisableKeyword("_ALPHATEST_ON");
                     mat.DisableKeyword("_ALPHABLEND_ON");
                     mat.EnableKeyword("_ALPHAPREMULTIPLY_ON");
                     mat.renderQueue = 3000;
+                }
+            }
+            foreach (MeshRenderer mr in MeshRend) {
+                if (mr) {
+                    mr.shadowCastingMode = ShadowCastingMode.Off;
                 }
             }
         }
@@ -40,14 +57,11 @@ public class HideChildren : MonoBehaviour {
         if (collider.tag == "MainCamera") {
             FadeIn = true;
             FadeOut = false;
-            /*foreach (Material mat in Material) {
-                if (mat) {
-                    
-                    Color saveColor = mat.color;
-                    saveColor.a = 1.0f;
-                    mat.color = saveColor;
+            foreach (MeshRenderer mr in MeshRend) {
+                if (mr) {
+                    mr.shadowCastingMode = ShadowCastingMode.On;
                 }
-            }*/
+            }
         }
     }
 
@@ -56,19 +70,12 @@ public class HideChildren : MonoBehaviour {
             foreach (Material mat in Material) {
                 if (mat) {
                     Color32 saveColor = mat.GetColor("_Color");
-                    if (saveColor.a != 255) {
+                    if (mat == LastMaterial && saveColor.a == 255) {
+                        SetOpaque();
+                        FadeIn = false;
+                    } else {
                         saveColor.a += 5;
                         mat.SetColor("_Color", saveColor);
-                    } else {
-                        mat.SetFloat("_Mode", 0);
-                        mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
-                        mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
-                        mat.SetInt("_ZWrite", 1);
-                        mat.DisableKeyword("_ALPHATEST_ON");
-                        mat.DisableKeyword("_ALPHABLEND_ON");
-                        mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
-                        mat.renderQueue = -1;
-                        FadeIn = false;
                     }
                 }
             }
@@ -77,10 +84,26 @@ public class HideChildren : MonoBehaviour {
                 if (mat) {
                     Color saveColor = mat.color;
                     if (saveColor.a != 0) {
-                        saveColor.a -= 0.1f;
+                        saveColor.a -= 0.07f;
                         mat.color = saveColor;
                     }
                 }
+            }
+        }
+    }
+
+    void SetOpaque() {
+        foreach (Material mat in Material) {
+            if (mat) {
+                Color32 saveColor = mat.GetColor("_Color");
+                mat.SetFloat("_Mode", 1);
+                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.One);
+                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.Zero);
+                mat.SetInt("_ZWrite", 1);
+                mat.DisableKeyword("_ALPHATEST_ON");
+                mat.DisableKeyword("_ALPHABLEND_ON");
+                mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+                mat.renderQueue = -1;
             }
         }
     }
